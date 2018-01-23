0c190f2ec49ca3712314d2c14a097727fefc83e2
ankur kathuria <ankur.kathuria2012@gmail.com>
2018-01-23 09:18:01 +0000

    gfbuildwrapper.sh edited online with Bitbucket

--- hudson-tools/gf-jenkins-infra/slave/generic/docker/files/gfbuildwrapper.sh
+++ hudson-tools/gf-jenkins-infra/slave/generic/docker/files/gfbuildwrapper.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
-mkdir -p ${WORKSPACE}
-if [ $? -eq 0 ]; then
-   cd ${WORKSPACE}
-else
-   echo "Unable to create directory ${WORKSPACE}"
-fi
+#mkdir -p ${WORKSPACE}
+#if [ $? -eq 0 ]; then
+cd ${WORKSPACE}
+#else
+ #  echo "Unable to create directory ${WORKSPACE}"
+#fi
 true | which java
 java -version | true
 shopt -s nocasematch
