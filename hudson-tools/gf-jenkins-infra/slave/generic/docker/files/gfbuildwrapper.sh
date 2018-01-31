db7e86ca8137d2548fcc67d16d5dca9bbc1f94fb
ankur-pc <ankur.kathuria@oracle.com>
2018-01-31 17:13:28 +0530

    added some debug

--- hudson-tools/gf-jenkins-infra/slave/generic/docker/files/gfbuildwrapper.sh
+++ hudson-tools/gf-jenkins-infra/slave/generic/docker/files/gfbuildwrapper.sh
@@ -7,7 +7,11 @@ else
 fi
 ls -l /scratch/
 ls -l /scratch/gf-hudson-tools/
+ls -l /scratch/gf-hudson-tools/hudson_tools/
+ls -l /scratch/gf-hudson-tools/hudson_tools/jobs/
+ls -l /scratch/gf-hudson-tools/hudson_tools/jobs/gf-generic-test-job
 ls -l /scratch/gf-hudson-tools/hudson_tools/jobs/gf-generic-test-job/bin/
+
 true | which java
 java -version | true
 shopt -s nocasematch
