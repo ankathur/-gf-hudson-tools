23f552c3351b258644913b58622c7307fcc5a53d
ankur kathuria <ankur.kathuria2012@gmail.com>
2018-01-31 19:00:31 +0000

    gf-generic-test-job.sh edited online with Bitbucket

--- hudson-tools/jobs/gf-generic-test-job/bin/gf-generic-test-job.sh
+++ hudson-tools/jobs/gf-generic-test-job/bin/gf-generic-test-job.sh
@@ -4,5 +4,7 @@ export PATH=/gf-hudson-tools/bin:${PATH}
 
 #rm -rf ${WORKSPACE}/* || true
 jps -mv
+ls -l ${PARENT_WS_PATH}/
+ls -l ${PARENT_WS_PATH}/bundles/
 scp -o "StrictHostKeyChecking no" ${PARENT_NODE}:${PARENT_WS_PATH}/bundles/gftest.sh .
 bash -ex gftest.sh run_test ${TEST_ID}
