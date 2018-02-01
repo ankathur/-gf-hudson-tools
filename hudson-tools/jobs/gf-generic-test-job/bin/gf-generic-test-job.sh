b03a3604091e5701b764b620ff4fbf2ea6be81a1
ankur kathuria <ankur.kathuria2012@gmail.com>
2018-02-01 09:13:55 +0000

    gf-generic-test-job.sh edited online with Bitbucket

--- hudson-tools/jobs/gf-generic-test-job/bin/gf-generic-test-job.sh
+++ hudson-tools/jobs/gf-generic-test-job/bin/gf-generic-test-job.sh
@@ -6,5 +6,5 @@ export PATH=/gf-hudson-tools/bin:${PATH}
 jps -mv
 ls -l ${PARENT_WS_PATH_CONTAINER}/
 ls -l ${PARENT_WS_PATH_CONTAINER}/bundles/
-#scp -o "StrictHostKeyChecking no" ${PARENT_NODE}:${PARENT_WS_PATH_CONTAINER}/bundles/gftest.sh . need correction
-bash -ex ${PARENT_WS_PATH_CONTAINER}/bundles/gftest.sh run_test ${TEST_ID}
+scp -vvv -o "StrictHostKeyChecking no" ${PARENT_NODE}:${PARENT_WS_PATH_CONTAINER}/bundles/gftest.sh .
+bash -ex gftest.sh run_test ${TEST_ID}
