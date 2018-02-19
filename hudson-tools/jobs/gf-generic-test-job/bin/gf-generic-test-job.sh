afc9fbcbf120d82a47c818b85829f8ad69810fcf
ankur kathuria <ankur.kathuria2012@gmail.com>
2018-02-19 18:24:49 +0000

    gf-generic-test-job.sh edited online with Bitbucket

--- hudson-tools/jobs/gf-generic-test-job/bin/gf-generic-test-job.sh
+++ hudson-tools/jobs/gf-generic-test-job/bin/gf-generic-test-job.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 env
 export PATH=/gf-hudson-tools/bin:${PATH}
-ls -l /etc/ssh/
-ls -l /etc/ssh/ssh_host_rsa_key
-cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
-chmod og-wx ~/.ssh/authorized_keys 
-
+ls ~/.ssh
+cat ~/.ssh/ssh_host_rsa_key.pub >> /scratch/host_ssh/authorized_keys
 #rm -rf ${WORKSPACE}/* || true
 jps -mv
 ls -l ${PARENT_WS_PATH_CONTAINER}/
 ls -l ${PARENT_WS_PATH_CONTAINER}/bundles/
-scp -vvv -o "StrictHostKeyChecking no" ${PARENT_NODE}:${PARENT_WS_PATH_CONTAINER}/bundles/gftest.sh .
+scp -vvv -o "StrictHostKeyChecking no" genie.dash@${PARENT_NODE}:${PARENT_WS_PATH_CONTAINER}/bundles/gftest.sh .
 bash -ex gftest.sh run_test ${TEST_ID}
